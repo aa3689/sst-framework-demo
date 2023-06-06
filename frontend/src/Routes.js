7a77e3caaaa5781b805b1c9909750f7d9ae5e544
Toni Hiidenvuo <aa3689@student.jamk.fi>
2023-06-06 16:07:05 +0300

    signup functionality added

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 export default function Links() {
   return (
@@ -10,6 +11,7 @@ export default function Links() {
       <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />;
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
