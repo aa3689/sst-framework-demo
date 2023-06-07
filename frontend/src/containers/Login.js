490352ba26997a68a44bada33219116f26c3c774
Toni Hiidenvuo <aa3689@student.jamk.fi>
2023-06-07 19:01:32 +0300

    fixed button size

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -58,7 +58,7 @@ export default function Login() {
         </Form.Group>
         <LoaderButton
           block="true"
-          size="lg"
+          size="sm"
           type="submit"
           isLoading={isLoading}
           disabled={!validateForm()}
